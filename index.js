cc55c78572b38f348f60c3b764d1db793bd6472b
Kellymugambi <kellymgambi@.com>
2022-11-28 02:50:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name:'', streetAddress: ''
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee [key]= value
+    return employee
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let realEmployee = {...employee}
+    realEmployee [key] = value
+    return realEmployee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let realEmployee = {...employee}
+    delete realEmployee[key]
+    return realEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
